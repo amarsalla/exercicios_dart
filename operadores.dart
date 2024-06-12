8afebed8eba5d18f11e8a893b7fb780dea23dd55
amarsalla <amarsalla@gmail.com>
2024-06-12 15:59:34 -0300

    Criando Classes

--- operadores.dart
+++ operadores.dart
@@ -1,8 +1,32 @@
-int exec(int a, int b, int Function(int, int) fn) {
-  return fn(a, b); // Isso é uma função
+
+
+imprimirProduto(int qtde, {required String nome, required double preco}) {
+  for (var i = 0; i < qtde; i++) {
+  print("O produto ${nome} tem preco R\$${preco}");
+  }
+}
+
+
+class Produto {
+  late String nome;
+  late double preco;
+
+  Produto(String nome, double preco) {
+    this.nome = nome;
+    this.preco = preco;
+
+  }
 }
 
 main() {
-  final r = exec(20, 30, (a, b) => a * b + 100); // Passando uma função como parametro
-  print('O resultado é $r!!!!!!');
+var p1 = Produto('Caneta', 4.99);
+var p2 = Produto('Geladeira', 1454.99);
+
+// print("O produto ${p1.nome} tem preco R\$${p1.preco}");
+// print("O produto ${p2.nome} tem preco R\$${p2.preco}");
+
+imprimirProduto(1, preco: p1.preco, nome: p1.nome);
+imprimirProduto(20, preco: p2.preco, nome: p2.nome);
+
 }
+
