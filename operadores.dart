50c38ec168a18c86aab195a54b3d6e437b62555d
amarsalla <amarsalla@gmail.com>
2024-06-12 14:55:29 -0300

    Passando função como parametro

--- operadores.dart
+++ operadores.dart
@@ -1,9 +1,8 @@
-int soma(int a, int b) {
-  return a + b;
+int exec(int a, int b, int Function(int, int) fn) {
+  return fn(a, b); // Isso é uma função
 }
+
 main() {
-  final r = soma(2, 3);
-  print('O valor da soma é: $r');
-
-
+  final r = exec(20, 30, (a, b) => a * b + 100); // Passando uma função como parametro
+  print('O resultado é $r!!!!!!');
 }
